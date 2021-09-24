547d2d37dce57c16e8f49a29a5abdfa84ba8c071
sashkkaa <54464899+sashkkaa@users.noreply.github.com>
2021-09-24 13:30:27 +0300

    lesson1

---
+++ GB_Lesson1/GB_Lesson1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+
+            // Имя пользователя задается путем ввода в консоли
+            var userName = Console.ReadLine();
+
+            // Текущая дата в формате ДД/ММ/ГГГГ
+            var today = DateTime.Today.ToString("D");
+
+            // Вывод сообщения пользователю
+            Console.WriteLine($"Привет {userName}, сегодня {today}!");
+        }
+    }
+}
