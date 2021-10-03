e017d13c4bf3e99c4b1ff66e165d321f0d1089d0
sashkkaa <54464899+sashkkaa@users.noreply.github.com>
2021-10-03 17:15:57 +0300

    Add files via upload

---
+++ GB_Lesson3_2/GB_Lesson3_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GB_Lesson3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Kolya", "Sashka", "Masha", "Petya", "Vasya" };
+            string[] contacts = {"qwerty@qwe.ru", "+8 (999) 123 45 67", "ytrewq@ewq.ru", "+7 (874) 345 24 96", "qazwsx@edc.ru"};
+            string[,] contactBook = new string[5, 2];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < contactBook.GetLength(0); i++)
+            {
+                for (int j = 0; j < contactBook.GetLength(1); j++)
+                {
+                    if (j == 0)
+                        contactBook[i, j] = names[rnd.Next(0, names.Length)];
+                    else
+                        contactBook[i, j] = contacts[rnd.Next(0, contacts.Length)];
+
+                    System.Console.Write($"{contactBook[i, j]} ");
+                }
+                System.Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ GB_Lesson3_3/GB_Lesson3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GB_Lesson3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любой произвольный текст");
+            var str = Console.ReadLine();
+            
+            // разбиваем строку на массив символов
+            char[] strReverse = str.ToCharArray();
+            // Переворачиваем последовательность символов в массиве
+            Array.Reverse(strReverse);
+
+            Console.WriteLine(strReverse);
+        }
+    }
+}
