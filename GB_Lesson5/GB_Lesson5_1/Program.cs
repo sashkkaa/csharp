0d5b81e5e0ff632acd04aa9c116a8f3d45de2a9c
sashkkaa <54464899+sashkkaa@users.noreply.github.com>
2021-10-12 05:41:14 +0300

    lesson 5 homework

---
+++ GB_Lesson5/GB_Lesson5_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace GB_Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text:");
+            string text = Console.ReadLine();
+            string fileName = "Lesson1.txt";
+            File.WriteAllText(fileName, text);
+        }
+    }
+}
---
+++ GB_Lesson5/GB_Lesson5_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace GB_Lesson5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            Console.WriteLine("Введите числа от 0 до 255");
+            Numbers numbers = new Numbers();
+            numbers.num = Convert.ToInt16(Console.ReadLine());
+            formatter.Serialize(new FileStream("binary.bin", FileMode.OpenOrCreate), numbers);
+        }
+    }
+
+    [Serializable]
+    public class Numbers
+    {
+        public int num { get; set; }
+    }
+}
+
